a51bf282a6f77dcb612c8c21d97655ea1296e2c4
Kyse7535 <ktevot@gmail.com>
2025-03-19 11:22:30 +0100

    day-4--test

--- day-4/src/main/java/org/example/day_4/Day4.java
+++ day-4/src/main/java/org/example/day_4/Day4.java
@@ -5,6 +5,8 @@
 package org.example.day_4;
 
 import org.example.day_4.project_1.FilterEvenNumber;
+import org.example.day_4.project_2.StringMap;
+import org.example.day_4.project_4.UseReduce;
 
 /**
  *
@@ -16,5 +18,14 @@ public class Day4 {
         FilterEvenNumber filterEvenNumber = new FilterEvenNumber();
         System.out.println("Numbers : " + filterEvenNumber.getNumbers());
         System.out.println("Filtered : " + filterEvenNumber.filter());
+
+        StringMap map = new StringMap();
+        System.out.println("Get length : " + map.getLength());
+        UseReduce reduce = new UseReduce();
+        System.out.println("Moyenne de " + reduce.getNotes() + " = : " + reduce.mean());
+
+        Grouping grouping = new Grouping();
+        System.out.println("Grouper par ville: "+ grouping.group());
+
     }
 }
